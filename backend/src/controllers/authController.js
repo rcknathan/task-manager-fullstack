1865efe907102e2bf3faa30e570fdf6b11855a86
Erick Nathan <ericknathan.dev@gmail.com>
2026-04-09 21:03:01 -0300

    Fix Errors

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,4 @@
-const pool = require('../db');
+const pool = require('..');
 const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,4 +1,4 @@
-const pool = require('../db');
+const pool = require('..');
 
 // Create task
 exports.createTask = async (req, res) => {
