11d10c26f6026333cbbb9c2986f9203dbfd51c3f
Erick Nathan <ericknathan.dev@gmail.com>
2026-04-09 21:20:37 -0300

    CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const testRoutes = require('./routes/testRoutes');
 const taskRoutes = require('./routes/taskRoutes');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,5 +13,11 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/test', testRoutes);
 app.use('/tasks', taskRoutes);
+
+app.use(cors({
+  origin: 'https://task-manager-fullstack-taupe.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 module.exports = app;
